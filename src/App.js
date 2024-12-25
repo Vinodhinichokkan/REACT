83e6cb52d4906356dbea3b4f8ff6cd217b596ab2
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-12-25 13:03:31 +0530

    exe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  function handleNameChange() {
+    const names = ["Earn","Grow","Give"];
+    const int = Math.floor(Math.random()*3);
+    return names(int)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Subscribe to Vino
-        </p>
-        <a
-            className="App-link" 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Subscribe to vino
+      <p>Let's {handleNameChange()}Money</p>
     </div>
   );
-}
-
+        
+  
+  }
 export default App;
